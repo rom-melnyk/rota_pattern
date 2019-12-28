bd5cf9d978d7f4e3d22e3b0c29c3132ce011c900
MethewsMuller <methews.muller@gmail.com>
2019-12-28 08:43:51 +0200

    parsing class modifiede

--- src/cli_arguments.h
+++ src/cli_arguments.h
@@ -12,64 +12,62 @@ class CLIarguments {
 
 public:
 
-	bool SetDout(int dout) { this->dout = dout; return 1; }
-	bool SetDin(int din) { this->din = din; return 1; }
-	bool SetPen(int pen) { this->pen = pen; return 1; }
-	int GetDout() { return this->dout; }
-	int GetDin() { return this->din; }
-	int GetPen() { return this->pen; }
+	void SetDoutValue(int dout) { this->dout = dout; }
+	void SetDinValue(int din) { this->din = din;}
+	void SetPenValue(int pen) { this->pen = pen;}
 
-	CLIarguments(int dout = 0, int din = 0, int pen = 0) { //def constructor
-		this->din = din;
-		this->dout = dout;
-		this->pen = pen;
+	int GetDoutValue() { return this->dout; }
+	int GetDinValue() { return this->din; }
+	int GetPenValue() { return this->pen; }
+
+	CLIarguments() { //def constructor
+		this->din = 0;
+		this->dout = 0;
+		this->pen = 0;
 	}
 	// --dout=120
-	void Parsing(char *arg) {
-		int size = strlen(arg) + 1;
-		char *buff = new char[size];
-		int j = 0, a = 0, p = 0;  //iterators or counters
+	void Parse(char *arg) {
+		int size = strlen(arg)+1;
+		char *tmpArgument = new char[size];
+		int tmpSize = 0, numberPartIndex= 0, namePartIndex = 0;  //iterators or counters
 
 
 		for (int i = 0; i < size; i++) {
 			if (arg[i] != '-' && arg[i] != '=') {
-				buff[j] = arg[i];
-				j++;
+				tmpArgument[tmpSize] = arg[i];
+				tmpSize++;
 			}
 		}
 
-		char* namePart = new char[j];
-		char* numberPart = new char[j];
+		char* namePart = new char[tmpSize];
+		char* numberPart = new char[tmpSize];
 
-		for (int i = 0; i < j; i++) {
-			if ((int)buff[i] >= 48 && (int)buff[i] <= 57) {
-				numberPart[a] = buff[i];
-				a++;
+		for (int i = 0; i < tmpSize; i++) {
+			if ((int)tmpArgument[i] >= 48 && (int)tmpArgument[i] <= 57) {
+				numberPart[numberPartIndex] = tmpArgument[i];
+				numberPartIndex++;
 			}
 			else {
-				namePart[p] = buff[i];
-				p++;
+				namePart[namePartIndex] = tmpArgument[i];
+				namePartIndex++;
 			}
 		}
-		p--;
 		int newNumber = atoi(numberPart);
-		if (strcmp(namePart, "dout") == 0) {
-			SetDout(newNumber);
-		}
-		else if (strcmp(namePart, "din") == 0) {
-			SetDin(newNumber);
-		}
-		else if (strcmp(namePart, "pen") == 0) {
-			SetPen(newNumber);
-		}
+		if (strcmp(namePart, "dout") == 0)
+			SetDoutValue(newNumber);
+		else if (strcmp(namePart, "din") == 0)
+			SetDinValue(newNumber);
+		else if (strcmp(namePart, "pen") == 0) 
+			SetPenValue(newNumber);
+
 		delete[] numberPart;
 		delete[]namePart;
-		delete[] buff;
+		delete[] tmpArgument;
 	}
 	void Show() {
-		cout << "Dout: " << GetDout() << endl;
-		cout << "Din: " << GetDin() << endl;
-		cout << "Pen: " << GetPen() << endl;
+		cout << "Dout: " << GetDoutValue() << endl;
+		cout << "Din: " << GetDinValue() << endl;
+		cout << "Pen: " << GetPenValue() << endl;
 	}
 };
 
--- src/rota_pattern.cpp
+++ src/rota_pattern.cpp
@@ -12,12 +12,11 @@ int main(int count, char* argum[]) {
 
 	cout << "\n----------------------------\n";
 
-	CLIarguments abc;
-
-	abc.Parsing(argum[1]);
-	abc.Parsing(argum[2]);
-	abc.Parsing(argum[3]);
-	abc.Show();
+	CLIarguments Argument;
+	for (int i = 1; i <count; i++)
+	Argument.Parse(argum[i]);
+	
+	Argument.Show();
 	
   return 0;
 }
